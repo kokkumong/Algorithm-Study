004b305b1bcde8bec73cd4b789b88ee839bcc570
kokkumong <102169091+kokkumong@users.noreply.github.com>
2025-07-11 16:57:37 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 505.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -2,24 +2,15 @@ def solution(n, m):
     answer = []
     #약수 찾기
     a = []
-    b = []
-    for i in range(1,n+1):
-        if(n % i == 0):
+    for i in range(1,max(n,m)):
+        if(n % i == 0 and m % i == 0):
             a.append(i)
-    for j in range(1,m+1):
-        if(m % j == 0):
-            b.append(j)
-    #최대공약수 구하기
-    GCD = max(set(a) & set(b))
-    answer.append(GCD)
+    answer.append(max(a))
     #배수 찾기
-    d = []
-    e = []
-    for i in range(1,m + 1):
-        d.append(n*i)
-    for j in range(1,n+1):
-        e.append(m*j)
+    b = []
+    for j in range(1,n*m + 1):
+        if(j % n == 0 and j % m == 0):
+            b.append(j)
+    answer.append(min(b))
     #최소 공배수 찾기
-    LCD = min(set(d) & set(e))
-    answer.append(LCD)
-    return answer
+    return(answer)
